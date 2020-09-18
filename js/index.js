1203d4da65df8fa8bdc219a3d69f7ba0a1a53a64
Blink98 <gs130899@gmail.com>
2020-09-18 23:56:36 +0530

    Changed the primary color of the page and added the extended sidebars.

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// Template button and it's extended sidebar
+const templateBtn = document.getElementById("templateBtn");
+const templateSidebarExt = document.getElementById("templateSidebarExt");
+
+templateBtn.addEventListener("click", function () { 
+    templateSidebarExt.classList.toggle("hide-element");
+});
+
+// Uplaod button and it's extended sidebar
+const uploadBtn = document.getElementById("uploadBtn");
+const uploadSidebarExt = document.getElementById("uploadSidebarExt");
+
+uploadBtn.addEventListener("click", function () { 
+    uploadSidebarExt.classList.toggle("hide-element");
+});
+
+// Elements button and it's extended sidebar
+const elementsBtn = document.getElementById("elementsBtn");
+const elementSidebarExt = document.getElementById("elementSidebarExt");
+
+elementsBtn.addEventListener("click", function () { 
+    elementSidebarExt.classList.toggle("hide-element");
+});
+
+// Text button and it's extended sidebar
+const textBtn = document.getElementById("textBtn");
+const textSidebarExt = document.getElementById("textSidebarExt");
+
+textBtn.addEventListener("click", function () { 
+    textSidebarExt.classList.toggle("hide-element");
+});
